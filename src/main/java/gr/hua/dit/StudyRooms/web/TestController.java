1e4b992d60ccc68e539bfb33093dc14a35483672
Rebeka Sidor <sidorrebeka@gmail.com>
2025-12-22 03:44:31 +0200

    6.3

--- src/main/java/gr/hua/dit/StudyRooms/web/TestController.java
+++ src/main/java/gr/hua/dit/StudyRooms/web/TestController.java
@@ -1,45 +1,33 @@
 package gr.hua.dit.StudyRooms.web;
 
-import gr.hua.dit.StudyRooms.core.model.Person;
-import gr.hua.dit.StudyRooms.core.model.PersonType;
-import gr.hua.dit.StudyRooms.core.repository.PersonRepository;
-
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * Controller for <strong>Testing</strong>.
+ */
+@Controller
 public class TestController {
 
-    private final PersonRepository personRepository;
+    public TestController() {}
 
-    public TestController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    /*
+    @GetMapping(value = "/test/error/404")
+    public String test() {
+        return "error/404";
     }
 
+    @GetMapping(value = "/test/error/error")
+    public String testErrorError() {
+        return "error/error";
+    }
+    */
 
-    @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String test() {
-        for (int i=0; i<100; i++) {
-            Person person1 = new Person();
-            person1.setId(null);
-            person1.setLibraryId("lib2025" + i);
-            person1.setFirstName("FN " + i);
-            person1.setLastName("LN" + i);
-            person1.setEmailAddress("student@gmail.com");
-            person1.setMobilePhoneNumber("+306900000000");
-            person1.setType(PersonType.STUDENT);
-            person1.setPasswordHash("<hash>");
-
-            person1 = this.personRepository.save(person1);
-        }
-
-        final var people = this.personRepository
-                .findByLibraryId("lib2025");
-
-        final String stringToServe = String.join(
-                "\n", people.stream().map(Person::toString).toList());
-
-        return stringToServe;
+    @GetMapping(value = "/test/error/NullPointerException")
+    public String testErrorNullPointerException() {
+        final Integer a = null;
+        final int b = 0;
+        final int c = a + b; // Throws NullPointerException.
+        return null; // Unreachable.
     }
 }
