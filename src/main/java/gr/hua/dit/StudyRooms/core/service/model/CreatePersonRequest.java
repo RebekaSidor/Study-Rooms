699aa0b35b93d57fac58592395b8d99ce325a95d
Rebeka Sidor <sidorrebeka@gmail.com>
2026-01-20 01:01:07 +0200

    invalid input fix

--- src/main/java/gr/hua/dit/StudyRooms/core/service/model/CreatePersonRequest.java
+++ src/main/java/gr/hua/dit/StudyRooms/core/service/model/CreatePersonRequest.java
@@ -1,20 +1,44 @@
 package gr.hua.dit.StudyRooms.core.service.model;
 
 import gr.hua.dit.StudyRooms.core.model.PersonType;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 /**
  * DTO for requesting the creation (registration) of a Person.
  */
 public record CreatePersonRequest(
-        @NotNull PersonType type,
-                           @Size(max = 20) String libraryId,
-        @NotNull @NotBlank @Size(max = 100) String firstName,
-        @NotNull @NotBlank @Size(max = 100) String lastName,
-        @NotNull @NotBlank @Size(max = 100) @Email String emailAddress,
-        @NotNull @NotBlank @Size(max = 18) String mobilePhoneNumber,
-        @NotNull @NotBlank @Size(min = 4, max = 24) String rawPassword
-){}
+        @NotNull(message = "person type is mandatory")
+        PersonType type,
+
+        @Size(max = 20)
+        String libraryId,
+
+        @NotBlank(message = "first name is mandatory")
+        @Size(max = 100)
+        @Pattern(
+                regexp = "^[A-Za-zΑ-Ωα-ωΆΈΉΊΌΎΏάέήίόύώ]+$",
+                message = "first name must have only letters"
+        )
+        String firstName,
+
+        @NotBlank(message = "last name is mandatory")
+        @Size(max = 100)
+        @Pattern(
+                regexp = "^[A-Za-zΑ-Ωα-ωΆΈΉΊΌΎΏάέήίόύώ]+$",
+                message = "last name must have only letters"
+        )
+        String lastName,
+
+        @NotBlank(message = "email is mandatory")
+        @Size(max = 100)
+        @Email(message = "non valid form of email")
+        String emailAddress,
+
+        @NotBlank(message = "phone number is mandatory")
+        @Size(max = 18)
+        String mobilePhoneNumber,
+
+        @NotBlank(message = "Ο κωδικός είναι υποχρεωτικός")
+        @Size(min = 4, max = 24, message = "password must have at least 4 characters")
+        String rawPassword
+) {}
